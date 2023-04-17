c0c858ee806eeaf65b1c1725e9690e3893dc8535
SeongWon Jeon <gateoper1105@gmail.com>
2023-04-17 21:26:24 +0900

    feat : 2.0 아이템 추가와 드랍아이템
    새로운 아이템을 만들었으며 아이템을 랜덤으로 드랍하게 만들었음

--- CollaboratorStudy/Item/Item.cs
+++ CollaboratorStudy/Item/Item.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CollaboratorStudy.Monster;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -7,17 +8,30 @@ namespace CollaboratorStudy.Item
     /// <summary>
     /// 아이템의 종류가 들어있습니다.
     /// </summary>
-    internal class Items
+    internal class Items : MonsterClass
     {
         public string name;
         public int price;
         public int money;
+        public string dropIT;
 
-        // 아이템을 야무지게 만들었따
+        // 아이템 큰구슬을 생성
         public void BigBead()
         {
-            this.name = ("큰구슬");
-            this.price = (3000);
+            this.name = "큰구슬";
+            this.price = 3000;
+        }
+        // 아이템 작은구슬을 생성
+        public void SmallBead()
+        {
+            this.name = "작은구슬";
+            this.price = 1500;
+        }
+        // 랜덤아이템 드랍
+        public void RandomDropItem()
+        {
+            Random randomDrop = new Random();
+            randomDrop.Next(money, name.Length);
         }
         // 플레이어가 아이템을 사용할 수 있게 만들었습니다.
         public virtual void Use() { }
@@ -25,9 +39,15 @@ namespace CollaboratorStudy.Item
         // 아이템에 돈을 만들었습니다.
         public void DropMoney()
         {
-            Random random = new Random();
-            this.money += random.Next(1, 999999);
+            Random randomMoney = new Random();
+            this.money += randomMoney.Next(1, 999999);
         }
         
+        // 드랍하는 아이템
+        public void DropItems()
+        {
+            Random randomItems = new Random();
+            RandomDropItem();
+        }
     }
 }
