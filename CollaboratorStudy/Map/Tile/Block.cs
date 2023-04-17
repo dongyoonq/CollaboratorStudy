151d0d4d68174ff30e3021a91ada3382cbb9e07a
동윤 김 <ut_community@naver.com>
2023-04-17 21:27:21 +0900

    fix : Tile Class fix
    Tile 클래스에서 불필요한 메서드 제거

---
+++ CollaboratorStudy/Map/Tile/Block.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dongyoon
+{
+    internal class Block : Tile
+    {
+        protected override void SetCollison()
+        {
+            IsCollisonTile = false;
+        }
+    }
+}
