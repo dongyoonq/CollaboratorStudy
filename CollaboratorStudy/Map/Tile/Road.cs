e2e7dc14aec95af8eaf391a343c07d55d143d1ff
동윤 김 <ut_community@naver.com>
2023-04-17 21:42:34 +0900

    feat : Add Bush,Road Class
    수풀과 길 클래스 추가

---
+++ CollaboratorStudy/Map/Tile/Road.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dongyoon
+{
+    internal class Road : Tile
+    {
+        public Road(int size) : base(size)
+        {
+            IsCollisonTile = true;
+        }
+    }
+}
