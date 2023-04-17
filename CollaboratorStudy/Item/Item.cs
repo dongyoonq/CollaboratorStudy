45d1d4e8cffa786ff42173f6696da288ffb74312
SeongWon Jeon <gateoper1105@gmail.com>
2023-04-17 20:35:40 +0900

    feat : Item의 종류와 돈을 드랍할 수 있게 만들었습니다.

--- CollaboratorStudy/Item/Item.cs
+++ CollaboratorStudy/Item/Item.cs
@@ -9,8 +9,30 @@ namespace CollaboratorStudy.Item
     /// </summary>
     internal class Items
     {
-        // 아이템을 야무지게 만들었따
+        public string name;
+        public int price;
+        public int money;
 
+        // 아이템 냐미
+        public virtual Item()
+        {
+            this.name = Coin
+            
+        }
+
+        // 아이템을 야무지게 만들었따
+        public void BigBead()
+        {
+            this.name = new string("큰구슬")
+            this.price = new int(3000);
+        }
         // 플레이어가 아이템을 사용할 수 있게 만들었습니다.
+        public void Use();
+
+        // 아이템에 돈을 만들었습니다.
+        public void DropMoney()
+        {
+            this.money = new int(1,999999)
+        }
     }
 }
