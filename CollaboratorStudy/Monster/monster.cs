1ae8f7d8f0d31b94f8950c50235273e284c3c3f8
윤진 소 <pms010506@gmail.com>
2023-04-17 20:16:04 +0900

    feat: monster의 기본 자료형/설정

--- CollaboratorStudy/Monster/monster.cs
+++ CollaboratorStudy/Monster/monster.cs
@@ -1,15 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Channels;
 
 namespace CollaboratorStudy.Monster
 {
-    internal class monster
+    internal class monster      // 몬스터 클래스입니다.
     {
-        public void pikapika()
+        public string name { get; set; }
+        public int age { get; set; }
+        public int hp { get; set; }
+        
+        public void pikapika()      
         {
             Console.WriteLine("피카츄 라이츄 파이리 꼬부기");
         }
+
+        
     }
 }
